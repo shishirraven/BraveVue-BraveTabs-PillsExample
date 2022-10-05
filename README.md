cc17a1a276f99a1feb1a13ef30bcfc434d0c8551
shishirraven <shishir.raven@gmail.com>
2022-10-05 08:53:32 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# BraveVue-BraveTabs-PillsExample
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-truwea)
---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="flex justify-center pb-5">
+      <BraveThemeSwitcher />
+    </div>
+    <main class="container mx-auto lg:px-32 mt-5">
+      <HorizontalPillTabs />
+    </main>
+  </div>
+</template>
+<script>
+import HorizontalPillTabs from './components/HorizontalPillTabs.vue';
+import BraveThemeSwitcher from './components/BraveThemeSwitcher.vue';
+export default {
+  components: { HorizontalPillTabs, BraveThemeSwitcher },
+};
+</script>
